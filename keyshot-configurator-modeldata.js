8aaa70b67b8a6a415b682e3ca39c54d9f561d8b4
carterjames99 <carterjames99@gmail.com>
2021-04-04 17:32:14 -0400

    Dolores Config

---
+++ keyshot-configurator-modeldata.js
@@ -0,0 +1,227 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "dark.png",
+                    "multimaterials": [
+                        "120+119",
+                        "48+47",
+                        "81+79",
+                        "86+84",
+                        "117+116"
+                    ],
+                    "name": "Dark"
+                },
+                {
+                    "checked": true,
+                    "icon": "light.png",
+                    "multimaterials": [
+                        "120+118",
+                        "48+47",
+                        "81+80",
+                        "86+85",
+                        "117+115"
+                    ],
+                    "name": "Light"
+                }
+            ],
+            "name": "Scene"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "stormtrooper%20white.png",
+                    "multimaterials": [
+                        "113+101"
+                    ],
+                    "name": "Stormtrooper White"
+                },
+                {
+                    "icon": "matte%20blackout.png",
+                    "multimaterials": [
+                        "113+102"
+                    ],
+                    "name": "Matte Blackout"
+                },
+                {
+                    "icon": "platinum%20grey.png",
+                    "multimaterials": [
+                        "113+103"
+                    ],
+                    "name": "Platinum Grey"
+                },
+                {
+                    "icon": "aztec%20teal.png",
+                    "multimaterials": [
+                        "113+104"
+                    ],
+                    "name": "Aztec Teal"
+                },
+                {
+                    "icon": "burnt%20bronze.png",
+                    "multimaterials": [
+                        "113+105"
+                    ],
+                    "name": "Burnt Bronze"
+                },
+                {
+                    "icon": "ridgeway%20blue.png",
+                    "multimaterials": [
+                        "113+106"
+                    ],
+                    "name": "Ridgeway Blue"
+                },
+                {
+                    "icon": "northern%20lights.png",
+                    "multimaterials": [
+                        "113+107"
+                    ],
+                    "name": "Northern Lights"
+                },
+                {
+                    "checked": true,
+                    "icon": "lollypop%20purple.png",
+                    "multimaterials": [
+                        "113+108"
+                    ],
+                    "name": "Lollypop Purple"
+                },
+                {
+                    "icon": "rose%20gold.png",
+                    "multimaterials": [
+                        "113+109"
+                    ],
+                    "name": "Rose Gold"
+                },
+                {
+                    "icon": "electric%20yellow.png",
+                    "multimaterials": [
+                        "113+110"
+                    ],
+                    "name": "Electric Yellow"
+                },
+                {
+                    "icon": "prison%20pink.png",
+                    "multimaterials": [
+                        "113+111"
+                    ],
+                    "name": "Prison Pink"
+                },
+                {
+                    "icon": "highland%20green.png",
+                    "multimaterials": [
+                        "113+112"
+                    ],
+                    "name": "Highland Green"
+                }
+            ],
+            "name": "Top"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "stormtrooper%20white-2.png",
+                    "multimaterials": [
+                        "100+88"
+                    ],
+                    "name": "Stormtrooper White"
+                },
+                {
+                    "icon": "matte%20blackout-2.png",
+                    "multimaterials": [
+                        "100+89"
+                    ],
+                    "name": "Matte Blackout"
+                },
+                {
+                    "icon": "platinum%20grey-2.png",
+                    "multimaterials": [
+                        "100+90"
+                    ],
+                    "name": "Platinum Grey"
+                },
+                {
+                    "icon": "aztec%20teal-2.png",
+                    "multimaterials": [
+                        "100+91"
+                    ],
+                    "name": "Aztec Teal"
+                },
+                {
+                    "icon": "burnt%20bronze-2.png",
+                    "multimaterials": [
+                        "100+92"
+                    ],
+                    "name": "Burnt Bronze"
+                },
+                {
+                    "icon": "ridgeway%20blue-2.png",
+                    "multimaterials": [
+                        "100+93"
+                    ],
+                    "name": "Ridgeway Blue"
+                },
+                {
+                    "icon": "northern%20lights-2.png",
+                    "multimaterials": [
+                        "100+94"
+                    ],
+                    "name": "Northern Lights"
+                },
+                {
+                    "icon": "lollypop%20purple-2.png",
+                    "multimaterials": [
+                        "100+95"
+                    ],
+                    "name": "Lollypop Purple"
+                },
+                {
+                    "checked": true,
+                    "icon": "rose%20gold-2.png",
+                    "multimaterials": [
+                        "100+96"
+                    ],
+                    "name": "Rose Gold"
+                },
+                {
+                    "icon": "electric%20yellow-2.png",
+                    "multimaterials": [
+                        "100+97"
+                    ],
+                    "name": "Electric Yellow"
+                },
+                {
+                    "icon": "prison%20pink-2.png",
+                    "multimaterials": [
+                        "100+98"
+                    ],
+                    "name": "Prison Pink"
+                },
+                {
+                    "icon": "highland%20green-2.png",
+                    "multimaterials": [
+                        "100+99"
+                    ],
+                    "name": "Highland Green"
+                }
+            ],
+            "name": "Bottom"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Scene",
+                "Top",
+                "Bottom"
+            ],
+            "modelset": 1006,
+            "name": "Default"
+        }
+    ],
+    "studios": []
+};
